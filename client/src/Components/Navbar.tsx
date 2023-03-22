f0d44b5f1d36f6837ff32ecccab20a6616c50a0e
mohamed montaser <mastrocoding@gmail.com>
2023-03-22 15:27:55 +0200

    add search button functionality

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { NotificationMajor, SearchMajor } from "@shopify/polaris-icons";
 import { Icon } from "@shopify/polaris";
 
-export default function Navbar() {
+export default function Navbar(): JSX.Element {
+  const [showSearchBar, setShowSearchBar] = useState<boolean>(false);
+  const searchRef = useRef<HTMLDivElement>(null);
+  const toggleSearchBar = (currentState: boolean) => {
+    console.log(currentState);
+    setShowSearchBar(!currentState);
+    console.log(showSearchBar);
+    let reverse = !currentState;
+    if (reverse) {
+      searchRef.current.classList.add("bottom");
+      setTimeout(() => {
+        searchRef.current.classList.add("z-100");
+      }, 400);
+    }
+    if (!reverse) {
+      searchRef.current.classList.remove("z-100");
+      setTimeout(() => {
+        searchRef.current.classList.add("top");
+        searchRef.current.classList.remove("bottom");
+      }, 180);
+    }
+  };
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container">
-          <div className="brand col-sm-4 col-md-6 d-flex justify-content-center align-items-center">
+          <div className="brand col-sm-4 d-flex justify-content-center align-items-center">
             <a className="navbar-brand" href="#">
               mohamed
             </a>
-            <div className="d-block">
+            <div
+              className="d-block"
+              onClick={() => toggleSearchBar(showSearchBar)}
+            >
               <Icon source={SearchMajor} color="base" />
             </div>
           </div>
-          <div className="d-flex col-sm-4 col-none justify-content-center">
-            <form role="search">
-              <input
-                className="form-control"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-            </form>
-          </div>
-          <div className="col-sm-4 col-md-6 d-flex justify-content-center">
+          <div className="col-sm-4 d-flex justify-content-center">
             <Icon source={NotificationMajor} color="base" />
             <img
               src={require("../images/profile pic.jpg")}
@@ -41,6 +55,25 @@ export default function Navbar() {
           </div>
         </div>
       </nav>
+      <div className="container">
+        <div className="row">
+          <div
+            className={`d-flex top justify-content-center search-bar col-sm-12`}
+            ref={searchRef}
+          >
+            <div>
+              <form role="search">
+                <input
+                  className="form-control"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
